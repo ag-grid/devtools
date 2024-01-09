7f8ffe53fd23635aad37cf33a234248d4005223d
Tim Kendrick <tim.kendrick@ag-grid.com>
2024-01-09 09:38:42 +0000

    Update vite build dependencies

--- packages/build-config/templates/vite/base.vite.config.ts
+++ packages/build-config/templates/vite/base.vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   build: {
     sourcemap: true,
+  },
+  test: {
+    coverage: {
+      exclude: [...(configDefaults.coverage.exclude || []), '**/__fixtures__/**'],
+    },
   },
 });
