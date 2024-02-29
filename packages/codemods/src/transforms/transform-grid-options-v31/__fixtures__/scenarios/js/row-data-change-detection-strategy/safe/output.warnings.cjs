782fb142d55a9078b4cd790e00854492beb1038a
Tim Kendrick <tim.kendrick@ag-grid.com>
2024-02-29 12:52:38 +0000

    Strip line numbers from error test scenarios

--- packages/codemods/src/transforms/transform-grid-options-v31/__fixtures__/scenarios/js/row-data-change-detection-strategy/safe/output.warnings.cjs
+++ packages/codemods/src/transforms/transform-grid-options-v31/__fixtures__/scenarios/js/row-data-change-detection-strategy/safe/output.warnings.cjs
@@ -1,9 +1,6 @@
 module.exports = [
   new SyntaxError(`The grid option "rowDataChangeDetectionStrategy" is deprecated. Please refer to the migration guide for more details: https://ag-grid.com/javascript-data-grid/upgrading-to-ag-grid-31/
-  4 |   columnDefs: [],
-  5 |   rowData: [],
-> 6 |   rowDataChangeDetectionStrategy: 'DeepEqualityCheck',
-    |   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  7 | });
-  8 |`),
+
+> |   rowDataChangeDetectionStrategy: 'DeepEqualityCheck',
+  |   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`),
 ];
--- packages/codemods/src/transforms/transform-grid-options-v31/__fixtures__/scenarios/react/on-column-row-group-change-request/safe/output.warnings.cjs
+++ packages/codemods/src/transforms/transform-grid-options-v31/__fixtures__/scenarios/react/on-column-row-group-change-request/safe/output.warnings.cjs
@@ -1,10 +1,6 @@
 module.exports = [
   new SyntaxError(`The grid option "onColumnRowGroupChangeRequest" is deprecated. Please refer to the migration guide for more details: https://ag-grid.com/javascript-data-grid/upgrading-to-ag-grid-31/
-  11 |         columnDefs={[]}
-  12 |         rowData={[]}
-> 13 |         onColumnRowGroupChangeRequest={onColumnRowGroupChangeRequest}
-     |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  14 |       />
-  15 |     </div>
-  16 |   );`),
+
+> |         onColumnRowGroupChangeRequest={onColumnRowGroupChangeRequest}
+  |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`),
 ];
