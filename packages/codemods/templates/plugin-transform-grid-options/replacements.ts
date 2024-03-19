44c13d1a4cdfbf3044569f75295a1ab0777f48d6
Tim Kendrick <tim.kendrick@ag-grid.com>
2024-03-19 12:43:12 +0000

    Fix create transform scaffolding task

--- packages/codemods/templates/plugin-transform-grid-options/replacements.ts
+++ packages/codemods/templates/plugin-transform-grid-options/replacements.ts
@@ -1,13 +1,13 @@
 import {
-  GridOptionReplacement,
-  getGridOptionReplacements,
   invertOptionalBooleanValue,
   migrateOptionalValue,
   migrateProperty,
   removeProperty,
+  transformObjectProperties,
+  type CodemodObjectPropertyReplacement,
 } from '../../plugins/<%= plugin %>/transform-grid-options';
 
-export const replacements: Array<GridOptionReplacement> = getGridOptionReplacements({
+export const replacements: Array<CodemodObjectPropertyReplacement> = transformObjectProperties({
   hello: migrateProperty('greet', migrateOptionalValue()),
   goodbye: removeProperty('The "goodbye" option has been removed. See release notes for upgrade instructions.'),
   friendly: migrateProperty('unfriendly', invertOptionalBooleanValue()),
