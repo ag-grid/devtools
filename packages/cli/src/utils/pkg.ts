452e237212cc88b1957e55f34defb00d746190b4
Tim Kendrick <tim.kendrick@ag-grid.com>
2024-03-21 11:01:31 +0000

    Fix ESLint warning

--- packages/cli/src/utils/pkg.ts
+++ packages/cli/src/utils/pkg.ts
@@ -1,7 +1,4 @@
-import pkg from '../../package.json' assert { type: 'json' };
-const { name, version } = pkg as { name: string; version: string };
-// FIXME: reinstate
-// import { name, version } from '../../package.json' assert { type: 'json' };
+import { name, version } from '../../package.json' assert { type: 'json' };
 
 export function getPackageName(): string {
   return name;
