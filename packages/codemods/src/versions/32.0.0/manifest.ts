a7249c2ab6daa9f689a841036540c952759b8d06
Stephen Cooper <scooperdev@gmail.com>
2024-06-26 15:42:07 +0100

    showLoadingOverlay deprecation

--- packages/codemods/src/versions/32.0.0/manifest.ts
+++ packages/codemods/src/versions/32.0.0/manifest.ts
@@ -2,7 +2,12 @@ import { type TransformManifest, type VersionManifest } from '@ag-grid-devtools/
 
 import transformGridOptionsV32_0 from '../../transforms/transform-grid-options-v32-0/manifest.ts';
 
-const transforms: Array<TransformManifest> = [transformGridOptionsV32_0];
+import transformGridApiMethodsV32_0 from '../../transforms/transform-grid-api-methods-v32-0/manifest.ts';
+
+const transforms: Array<TransformManifest> = [
+  transformGridOptionsV32_0,
+  transformGridApiMethodsV32_0,
+];
 
 const manifest: VersionManifest = {
   version: '32.0.0',
