412eb45399c7920dac19d51d5c02ecbc916dc33d
Stephen Cooper <scooperdev@gmail.com>
2024-06-26 14:32:36 +0100

    Setup v32

---
+++ packages/codemods/src/versions/32.0.0/manifest.ts
@@ -0,0 +1,11 @@
+import { type TransformManifest, type VersionManifest } from '@ag-grid-devtools/types';
+
+const transforms: Array<TransformManifest> = [];
+
+const manifest: VersionManifest = {
+  version: '32.0.0',
+  codemodPath: 'version/32.0.0',
+  transforms,
+};
+
+export default manifest;
