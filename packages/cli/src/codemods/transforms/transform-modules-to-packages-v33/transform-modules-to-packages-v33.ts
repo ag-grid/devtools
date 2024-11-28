db027cd824e5cb92ce3e96992e56452b41b51a0e
Stephen Cooper <scooperdev@gmail.com>
2024-11-28 15:46:28 +0000

    use shared jsCodeShiftTransform

--- packages/cli/src/codemods/transforms/transform-modules-to-packages-v33/transform-modules-to-packages-v33.ts
+++ packages/cli/src/codemods/transforms/transform-modules-to-packages-v33/transform-modules-to-packages-v33.ts
@@ -1,10 +1,10 @@
 import { type AstCliContext, type AstTransform } from '@ag-grid-devtools/ast';
 
-import { jsCodeShiftTransform } from './jscodeshift.adapter';
+import { jsCodeShiftTransform } from '../../plugins/jscodeshift';
 import { processImports } from './transformers/module-imports';
 
 const transform: AstTransform<AstCliContext> = function transformModulesToPackagesV33(babel) {
-  return jsCodeShiftTransform(processImports);
+  return jsCodeShiftTransform(processImports)(babel);
 };
 
 export default transform;
