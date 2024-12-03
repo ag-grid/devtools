d74aa13350d45e4f6e16abaf7cc69fa1a3aa5ad4
Steph Meslin-Weber <steph@tangency.co.uk>
2024-12-03 12:43:51 +0000

    AG-13294 - warn when bar/column sparkline migration can't be done

--- packages/cli/src/codemods/transforms/transform-sparklines-options-v33-0/__fixtures__/scenarios/sparkline-column/output.ts
+++ packages/cli/src/codemods/transforms/transform-sparklines-options-v33-0/__fixtures__/scenarios/sparkline-column/output.ts
@@ -1,4 +1,5 @@
 // @ts-nocheck
+import { testType } from './test-type';
 const column = {
     cellRendererParams: {
         sparklineOptions: {
@@ -30,5 +31,35 @@ const shouldntChange1 = {
 const shouldntChange2 = {
     sparklineOptions: {
         type: 'asdadasqda'
+    }
+}
+const typeVariable = 'column'
+const shouldWarn1 = {
+    cellRendererParams: {
+        sparklineOptions: {
+            type: typeVariable
+        }
+    }
+}
+const type = 'column'
+const shouldWarn2 = {
+    cellRendererParams: {
+        sparklineOptions: {
+            type
+        }
+    }
+}
+const shouldWarn3 = {
+    cellRendererParams: {
+        sparklineOptions: {
+            type: (() => 'column')()
+        }
+    }
+}
+const shouldWarn4 = {
+    cellRendererParams: {
+        sparklineOptions: {
+            type: testType
+        }
     }
 }
--- packages/cli/src/codemods/transforms/transform-sparklines-options-v33-0/transformers/sparkline-column.ts
+++ packages/cli/src/codemods/transforms/transform-sparklines-options-v33-0/transformers/sparkline-column.ts
@@ -2,15 +2,26 @@ import j from 'jscodeshift';
 import { JSCodeShiftTransformer } from '../../../plugins/jscodeshift';
 
 // update bar/column types to bar with matching direction
-export const columnToVerticalBarTransform: JSCodeShiftTransformer = (root) =>
+export const columnToVerticalBarTransform: JSCodeShiftTransformer = (root, state) =>
   root
     .find(j.ObjectProperty, { key: { name: 'cellRendererParams' } })
     .find(j.ObjectProperty, { key: { name: 'sparklineOptions' } })
     .find(j.ObjectProperty, { key: { name: 'type' } })
-    .filter((path) => ['bar', 'column'].includes((path.value.value as any).value))
     .forEach((path) => {
-      const existingType = (path.value.value as any).value;
-      const direction = existingType === 'column' ? 'vertical' : 'horizontal';
-      path.replace(j.objectProperty(j.identifier('type'), j.stringLiteral('bar')));
-      path.insertAfter(j.objectProperty(j.identifier('direction'), j.stringLiteral(direction)));
+      const typeValue = path.value.value;
+      const isStringLiteral = j.StringLiteral.check(typeValue);
+      if (isStringLiteral && ['bar', 'column'].includes(typeValue.value)) {
+        const existingType = typeValue.value;
+        const direction = existingType === 'column' ? 'vertical' : 'horizontal';
+        path.replace(j.objectProperty(j.identifier('type'), j.stringLiteral('bar')));
+        path.insertAfter(j.objectProperty(j.identifier('direction'), j.stringLiteral(direction)));
+      } else if (!isStringLiteral) {
+        state?.opts.warn(
+          null,
+          new SyntaxError(
+            `The grid cellRendererParams sparklineOption "type" cannot be automatically migrated. Please refer to the migration guide for more details: https://ag-grid.com/javascript-data-grid/upgrading-to-ag-grid-33-0/`,
+          ),
+          { forceOutput: true },
+        );
+      }
     });
