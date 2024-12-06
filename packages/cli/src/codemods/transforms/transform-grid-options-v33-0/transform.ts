c6977684ce81970fb9fd5ad02a4519d1ab9304fb
Andrew Glazier <andrew.glazier@ag-grid.com>
2024-12-06 09:26:42 +0000

    AG-13133 - Delete colDef transforms

--- packages/cli/src/codemods/transforms/transform-grid-options-v33-0/transform.ts
+++ packages/cli/src/codemods/transforms/transform-grid-options-v33-0/transform.ts
@@ -2,14 +2,11 @@ import { type AstCliContext, type AstTransform, type Babel } from '@ag-grid-devt
 import { jsCodeShiftTransform } from '../../plugins/jscodeshift';
 import { transformGroupHideParentOfSingleChild } from './transformers/transformGroupHideParentOfSingleChild';
 import { transformSuppressGroupChangesColumnVisibility } from './transformers/transformSuppressGroupChangesColumnVisibility';
-import { transformGridOptionToColDef } from './transformers/transformGridOptionToColDef';
 
 const transform: AstTransform<AstCliContext> = (babel: Babel) => {
   return jsCodeShiftTransform(
     transformGroupHideParentOfSingleChild,
     transformSuppressGroupChangesColumnVisibility,
-    transformGridOptionToColDef('unSortIcon'),
-    transformGridOptionToColDef('sortingOrder'),
   )(babel);
 };
 
