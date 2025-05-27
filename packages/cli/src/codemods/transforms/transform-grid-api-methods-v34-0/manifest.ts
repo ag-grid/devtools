2c4af87aed7a436b0c0e6303237ad7ef396cb6ad
Elias Malik <elias.malik@ag-grid.com>
2025-05-27 22:17:30 +0100

    fix manifests

---
+++ packages/cli/src/codemods/transforms/transform-grid-api-methods-v34-0/manifest.ts
@@ -0,0 +1,8 @@
+import { type TransformManifest } from '@ag-grid-devtools/types';
+
+const manifest: TransformManifest = {
+  name: 'Transform Grid API Methods v34.0',
+  description: 'Transform deprecated Grid API methods',
+};
+
+export default manifest;
