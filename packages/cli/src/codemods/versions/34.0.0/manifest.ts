d23d9cf3b7551344d24c8ecde63f96b6c215fb3c
Elias <elias0789@gmail.com>
2025-06-24 04:49:46 -0700

    RTI-2834 Add version 34 (#114)
    * add version 34
    
    * increment package

---
+++ packages/cli/src/codemods/versions/34.0.0/manifest.ts
@@ -0,0 +1,11 @@
+import { type TransformManifest, type VersionManifest } from '@ag-grid-devtools/types';
+
+const transforms: Array<TransformManifest> = [];
+
+const manifest: VersionManifest = {
+  version: '34.0.0',
+  codemodPath: 'versions/34.0.0',
+  transforms,
+};
+
+export default manifest;
